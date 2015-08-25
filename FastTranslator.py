b1bb803e6aba88f6554414a3e07af080df7f5f03
Dary <anyofyou@gmail.com>
2015-08-25 16:29:26 +0800

    Add continuous mode

--- FastTranslator.py
+++ FastTranslator.py
@@ -29,48 +29,54 @@ def is_chinese(uchar):
 KEYFROM = 'YDTranslateTest'
 APIKEY = '1826356811'
 
+def translate(word):
+	r = requests.get('http://fanyi.youdao.com/openapi.do?keyfrom=' + KEYFROM +
+	'&key=' + APIKEY + '&type=data&doctype=json&version=1.1&q=' + word)
+	#print r.text
+	jsonDict = json.loads(r.text)
+	jsonStr = json.dumps(jsonDict, ensure_ascii=False,sort_keys=True,indent=4, separators=(',', ': '))
+	#print jsonStr
+
+	translation = jsonDict['translation'][0].encode('utf-8')
+	linebuf = StringIO.StringIO()
+	linebuf.write("%s%s%s " % (format(fg=MAGENTA,bg=None,bright=True), translation, format(reset=True)))
+	line = linebuf.getvalue()
+	print line
+	# os.popen('echo ' + translation + " | " + "pbcopy")
+	#script = "osascript -e " + "\'display notification " + "\"" + translation + "\"" + " with title " + "\"" + word + "\"" + "\'"
+	script = "terminal-notifier -title FastTranslator " + "-subtitle " + "\"" + word + "\"" + " -message " + "\"" + translation + "\"" + " -sender " + "\"com.googlecode.iterm2\""
+	#print script
+	os.popen(script)
+	try:
+		phonetic = jsonDict['basic']['phonetic']
+		linebuf = StringIO.StringIO()
+		linebuf.write("%s%s%s " % (format(fg=YELLOW,bg=None,bright=True), phonetic.encode('utf-8'), format(reset=True)))
+		print linebuf.getvalue()
+		#print phonetic.encode('utf-8')
+	except:
+		pass
+	try:
+		explains = jsonDict['basic']['explains']
+		for e in explains:
+			print e.encode('utf-8')
+	except:
+		pass
+
+	if is_chinese(unicode(word,'utf-8')):
+		if not is_chinese (unicode(translation,'utf-8')):
+			os.popen('say ' + translation)
+	else:
+		os.popen('say ' + word)
+
 try:
 	word = sys.argv[1]
+	translate(word)
 except IndexError:
-	print 'Error'
-	sys.exit()
-
-r = requests.get('http://fanyi.youdao.com/openapi.do?keyfrom=' + KEYFROM +
-'&key=' + APIKEY + '&type=data&doctype=json&version=1.1&q=' + word)
-#print r.text
-jsonDict = json.loads(r.text)
-jsonStr = json.dumps(jsonDict, ensure_ascii=False,sort_keys=True,indent=4, separators=(',', ': '))
-#print jsonStr
-
-translation = jsonDict['translation'][0].encode('utf-8')
-linebuf = StringIO.StringIO()
-linebuf.write("%s%s%s " % (format(fg=MAGENTA,bg=None,bright=True), translation, format(reset=True)))
-line = linebuf.getvalue()
-print line
-os.popen('echo ' + translation + " | " + "pbcopy")
-#script = "osascript -e " + "\'display notification " + "\"" + translation + "\"" + " with title " + "\"" + word + "\"" + "\'"
-script = "terminal-notifier -title FastTranslator " + "-subtitle " + "\"" + word + "\"" + " -message " + "\"" + translation + "\"" + " -sender " + "\"com.googlecode.iterm2\""
-#print script
-os.popen(script)
-
-try:
-	phonetic = jsonDict['basic']['phonetic']
-	linebuf = StringIO.StringIO()
-	linebuf.write("%s%s%s " % (format(fg=YELLOW,bg=None,bright=True), phonetic.encode('utf-8'), format(reset=True)))
-	print linebuf.getvalue()
-	#print phonetic.encode('utf-8')
-except:
-	pass
-
-try:
-	explains = jsonDict['basic']['explains']
-	for e in explains:
-		print e.encode('utf-8')
-except:
-	pass
-
-if is_chinese(unicode(word,'utf-8')):
-	if not is_chinese (unicode(translation,'utf-8')):
-		os.popen('say ' + translation)
-else:
-	os.popen('say ' + word)
+	print 'Enter text to translate, Ctrl-D to exit.\n'
+	try:
+		while True:
+			word = raw_input()
+			translate(word)
+			print '\n'
+	except:
+		pass
