c8e567827a6852ce0f78ef942132c194657a6151
Lucas Liu <anyofyou@gmail.com>
2016-05-11 18:34:08 +0800

    Update help

--- FastTranslator.py
+++ FastTranslator.py
@@ -114,7 +114,7 @@ parser.add_argument('-n', "--noti", help="show notification, need terminal-notif
 parser.add_argument('-d', "--debug", help="debug mode", action="store_true")
 parser.add_argument('-v', "--verbose", help="verbose output", action="store_true")
 parser.add_argument('-s', "--say", help="say the result", action="store_true")
-parser.add_argument("text", nargs='*')
+parser.add_argument("text",help="translated words, empty to enter interactive mode", nargs='*')
 args = parser.parse_args()
 # print args.verbose
 wordlist = ' '.join(args.text)
