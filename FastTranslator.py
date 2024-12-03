aa4757429b820e4c3d6b84fb9f4c11160cb58ff4
Lucas Liu <anyofyou@gmail.com>
2024-12-03 11:26:18 +0800

    Update

--- FastTranslator.py
+++ FastTranslator.py
@@ -10,6 +10,7 @@ import time
 import hashlib
 import time
 import uuid
+import re
 
 try:
     from StringIO import StringIO
@@ -207,7 +208,6 @@ def translate_youdao(text):
     r = requests.get('http://mobile.youdao.com/dict?le=eng&q=' + text)
     request_text = r.text
     # print(request_text)
-    import re
     phonetic_pattern = r'"phonetic">(.+)<\/span>'
     try:
         phonetic = re.findall(phonetic_pattern, request_text)[-1].replace('[', '').replace(']', '')
@@ -342,7 +342,29 @@ def print_result(args, text, result):
         say_result(text, translation)
 
 
+def cleanup_text(text):
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"
+        "\U0001F300-\U0001F5FF"
+        "\U0001F680-\U0001F6FF"
+        "\U0001F700-\U0001F77F"
+        "\U0001F780-\U0001F7FF"
+        "\U0001F800-\U0001F8FF"
+        "\U0001F900-\U0001F9FF"
+        "\U0001FA00-\U0001FA6F"
+        "\U0001FA70-\U0001FAFF"
+        "\U00002600-\U000026FF"
+        "\U00002700-\U000027BF"
+        "]+",
+        flags=re.UNICODE,
+    )
+    return emoji_pattern.sub(r'', text)
+
+
 def translate(args, text):
+    text = cleanup_text(text)
+    # print(text)
     if args.only_say:
         os.popen('say ' + '"' + text + '"')
         return
